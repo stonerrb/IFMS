0a4105f4ecdac14b952f4840866dec8fea17a74b
Rushabh Babre <babrerushabh1@gmail.com>
2024-10-25 03:17:05 +0530

    deployment push

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
 
 mongoose.connect(
-    process.env.MONGO_URI,
+    // process.env.MONGO_URI,
+    MONGO_URI
 )
 
 const db = mongoose.connection;
